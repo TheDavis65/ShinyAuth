41b53f6e3307c85e578d90d021e42509429ecd88
Michael Davis <michaeldavis@mail.dk>
2022-01-07 19:27:00 +0100

    first commit

---
+++ global.R
@@ -0,0 +1,10 @@
+library(shiny)
+library(polished)
+library(config)
+
+ # configur polished
+
+global_sessions_config(
+  app_name = "ShinyAuth",
+  api_key = "n64qxLsQvRVbUZNwW8j9xwtWGggLCRDDUc"
+)
